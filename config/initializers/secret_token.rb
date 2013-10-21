7f4b1dccf0d41eff8da61598db9defb0327e6d72
Adam Niedzielski <adamsunday@gmail.com>
2013-10-21 19:17:33 +0200

    rails-api skeleton with dummy user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Narwhale::Application.config.secret_key_base = '2d983b3bbbb89abc245909e05c7b893554a96cede1eeac390c1659d4375bc79e12726165eafc0b69d39aef512e29d01aade6defc4da43c88515482114e898862'
